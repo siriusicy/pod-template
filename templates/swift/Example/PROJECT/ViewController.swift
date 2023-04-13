ddfaf3358c2268e7ac3a73c9a467b4adb99d4022
ChenJie <siriusicy@gmail.com>
2023-04-13 10:34:56 +0800

    默认显示tableview

--- templates/swift/Example/PROJECT/ViewController.swift
+++ templates/swift/Example/PROJECT/ViewController.swift
@@ -12,13 +12,63 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        tableView.frame = CGRect(x: 0, y: 0,
+                                 width: UIScreen.main.bounds.width,
+                                 height: UIScreen.main.bounds.height)
+        view.addSubview(tableView)
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
+    // MARK: -  lazy
+    lazy var tableView : UITableView = {
+        
+        let tableView = UITableView(frame: .zero, style: .grouped)
+        tableView.separatorStyle = .none
+        tableView.backgroundColor = .clear
+        tableView.separatorStyle = .singleLine
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+        tableView.tableHeaderView = UIView(frame: CGRect(x: 0, y: 0, width: 1000, height: 0.01))
+        tableView.tableFooterView = UIView(frame: CGRect(x: 0, y: 0, width: 1000, height: 0.01))
+        tableView.sectionHeaderHeight = 0.01
+        tableView.sectionFooterHeight = 0.01
+        
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: NSStringFromClass(UITableViewCell.self))
+        
+        return tableView
+    }()
 }
+
+extension ViewController : UITableViewDelegate,UITableViewDataSource {
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 40
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: NSStringFromClass(UITableViewCell.self)) as! UITableViewCell
+
+        cell.textLabel?.text = "-=-=: \(indexPath.row)"
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: false)
+        
+    }
+    
+}
+
 
