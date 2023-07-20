6f76d0bb2f2a6a87d2397d02b331b9ada8dd321f
chenjie <siriusicy@gmail.com>
2023-07-20 15:15:56 +0800

    update

--- templates/ios/Example/podVariables.rb
+++ templates/ios/Example/podVariables.rb
@@ -1,12 +1,12 @@
 def git_source; return 'https://github.com/CocoaPods/Specs.git'; end
-def private_source; return 'https://jihulab.com/yaya-ios/yayaspec.git'; end
-def aliyun_source; return 'https://github.com/aliyun/aliyun-specs.git' end
-def local; return 'local'; end
-def remote; return 'remote'; end
-def gitee; return 'https://jihulab.com/yaya-ios/'; end
+##
+def gitlab_url; return 'http://gitlab.yunwei.love9w.com/ios/'; end
+def github_url; return 'https://github.com/siriusicy/'; end
+
+
 
 def remotePod(name)
-   pod name, :git => gitee+"#{name}.git",:branch=>'dev'
+   pod name, :git => gitlab_url+'#{name}.git',:branch=>'dev'
 end
 
 def _localPod(name)
